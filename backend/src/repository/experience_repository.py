f21e9253e4be1e1e4684d0f125e49f2bb4391353
SudiMango <sudipto.islam2005@gmail.com>
2026-03-26 13:26:18 -0700

    Added crud repository super class

--- backend/src/repository/experience_repository.py
+++ backend/src/repository/experience_repository.py
@@ -1,31 +1,7 @@
-from uuid import UUID
-from sqlalchemy.orm import Session
-from typing import List, Optional
-from src.schema.experience_schemas import ExperienceResponse, UpdateExperienceRequest
+from src.repository.crud_repository import CrudRepository
+from src.schema.experience_schemas import UpdateExperienceRequest
 from src.model.experience import Experience
 
-class ExperienceRepository:
-    def create(self, db: Session, entry: Experience) -> ExperienceResponse:
-        db.add(entry)
-        db.commit()
-        db.refresh(entry)
-        return entry
-    
-    def get_all(self, db: Session) -> List[ExperienceResponse]:
-        return db.query(Experience).all()
-
-    def get_one(self, db: Session, experience_id: UUID) -> Optional[ExperienceResponse]:
-        return db.query(Experience).filter(Experience.experience_id == experience_id).first()
-
-    def update(self, db: Session, experience: Experience, request: UpdateExperienceRequest) -> ExperienceResponse:
-        update_data = request.model_dump(exclude_unset=True)
-        for key, value in update_data.items():
-            setattr(experience, key, value)
-
-        db.commit()
-        db.refresh(experience)
-        return experience
-
-    def delete(self, db: Session, entry: Experience) -> None:
-        db.delete(entry)
-        db.commit()
+class ExperienceRepository(CrudRepository[Experience, UpdateExperienceRequest]):
+    def __init__(self):
+        super().__init__(Experience)
--- backend/src/repository/role_repository.py
+++ backend/src/repository/role_repository.py
@@ -1,31 +1,10 @@
 from uuid import UUID
 from sqlalchemy.orm import Session
 from typing import List, Optional
+from src.repository.crud_repository import CrudRepository
 from src.model.role import Role
 from src.schema.role_schemas import RoleResponse, UpdateRoleRequest
 
-class RoleRepository:
-    def create(self, db: Session, entry: Role) -> RoleResponse:
-        db.add(entry)
-        db.commit()
-        db.refresh(entry)
-        return entry
-    
-    def get_all(self, db: Session, experience_id: UUID) -> List[RoleResponse]:
-        return db.query(Role).filter(Role.experience_id == experience_id).all()
-
-    def get_one(self, db: Session, role_id: UUID) -> Optional[RoleResponse]:
-        return db.query(Role).filter(Role.role_id == role_id).first()
-
-    def update(self, db: Session, role: Role, request: UpdateRoleRequest) -> RoleResponse:
-        update_data = request.model_dump(exclude_unset=True)
-        for key, value in update_data.items():
-            setattr(role, key, value)
-
-        db.commit()
-        db.refresh(role)
-        return role
-
-    def delete(self, db: Session, entry: Role) -> None:
-        db.delete(entry)
-        db.commit()
+class RoleRepository(CrudRepository[Role, UpdateRoleRequest]):
+    def __init__(self):
+        super().__init__(Role)
