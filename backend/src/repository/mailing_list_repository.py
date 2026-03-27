38615253e287c4f10f4ac56a40c82bdf90e283c9
SudiMango <sudipto.islam2005@gmail.com>
2026-03-27 10:27:33 -0700

    Implemented crud repository for mailing list

--- backend/src/repository/mailing_list_repository.py
+++ backend/src/repository/mailing_list_repository.py
@@ -1,22 +1,17 @@
 from uuid import UUID
 from sqlalchemy.orm import Session
 from typing import Optional
+from pydantic import BaseModel
+from src.repository.crud_repository import CrudRepository
 from src.model.mailing_list import MailingList
 
 
-class MailingListRepository:
-    def create(self, db: Session, entry: MailingList) -> MailingList:
-        db.add(entry)
-        db.commit()
-        db.refresh(entry)
-        return entry
-
-    def delete(self, db: Session, entry: MailingList) -> None:
-        db.delete(entry)
-        db.commit()
+class MailingListRepository(CrudRepository[MailingList, BaseModel]):
+    def __init__(self):
+        super().__init__(MailingList)
 
     def find_by_email(self, db: Session, email: str) -> Optional[MailingList]:
-        return db.query(MailingList).filter(MailingList.email == email).first()
+        return db.query(self.model).filter(self.model.email == email).first()
 
     def find_by_id_and_email(self, db: Session, id: UUID, email: str) -> Optional[MailingList]:
-        return db.query(MailingList).filter(MailingList.mailing_list_id == id, MailingList.email == email).first()
+        return db.query(self.model).filter(self.model.mailing_list_id == id, self.model.email == email).first()
